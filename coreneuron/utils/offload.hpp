f49d0cde8ef504742658a6a5701b273bef406fb7
Olli Lupton <oliver.lupton@epfl.ch>
2021-12-07 16:47:03 +0100

    Declare cnrn_ methods.

--- coreneuron/utils/offload.hpp
+++ coreneuron/utils/offload.hpp
@@ -6,6 +6,8 @@
 # =============================================================================
 */
 #pragma once
+#include <cstddef>
+
 #define nrn_pragma_stringify(x) #x
 #if defined(CORENEURON_ENABLE_GPU) && defined(CORENEURON_PREFER_OPENMP_OFFLOAD) && defined(_OPENMP)
 #define nrn_pragma_acc(x)
@@ -18,3 +20,10 @@
 #define nrn_pragma_acc(x)
 #define nrn_pragma_omp(x)
 #endif
+
+namespace coreneuron {
+void* cnrn_gpu_copyin(void* h_ptr, std::size_t len);
+void cnrn_memcpy_to_device(void* d_ptr, void* h_ptr, std::size_t len);
+void cnrn_target_delete(void* h_ptr, std::size_t len);
+void* cnrn_target_deviceptr(void* h_ptr);
+} // namespace coreneuron
