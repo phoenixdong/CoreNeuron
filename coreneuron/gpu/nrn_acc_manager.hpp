78081b435ba165fb5e8ed58adaa2bc30d65a334b
Nicolas Cornu <nicolas.cornu@epfl.ch>
2021-12-14 08:48:12 +0100

    Remove unused GPU code (#711)
    We prefer selective host-to-device updates.

--- coreneuron/gpu/nrn_acc_manager.hpp
+++ coreneuron/gpu/nrn_acc_manager.hpp
@@ -19,13 +19,13 @@ namespace coreneuron {
 void setup_nrnthreads_on_device(NrnThread* threads, int nthreads);
 void delete_nrnthreads_on_device(NrnThread* threads, int nthreads);
 void update_nrnthreads_on_host(NrnThread* threads, int nthreads);
-void update_nrnthreads_on_device(NrnThread* threads, int nthreads);
-void modify_data_on_device(NrnThread* threads, int nthreads);
-void dump_nt_to_file(char* filename, NrnThread* threads, int nthreads);
 
 void update_net_receive_buffer(NrnThread* _nt);
+
+// Called by NModl
 void realloc_net_receive_buffer(NrnThread* nt, Memb_list* ml);
 void update_net_send_buffer_on_host(NrnThread* nt, NetSendBuffer_t* nsb);
+
 void update_weights_from_gpu(NrnThread* threads, int nthreads);
 void init_gpu();
 
